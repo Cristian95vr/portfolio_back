033ece9195b1e8ce6a904c7d009c29e533242981
Cristian95vr <cristianvegareyes95@gmail.com>
2024-01-13 05:38:06 -0300

    cambios

--- src/main/java/com/portfolio/cristian95vr/Controller/CEducacion.java
+++ src/main/java/com/portfolio/cristian95vr/Controller/CEducacion.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://portfolio-frontend-c42b6.web.app")
 public class CEducacion {
     @Autowired
     Seducacion sEducacion;
